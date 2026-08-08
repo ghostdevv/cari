aa03138b51f82ac1ab612a0290a11319e3094ef4
Willow (GHOST) <git@willow.sh>
2026-08-08 21:22:16 +0100

    perf: run outdated command work concurrently

--- src/commands/outdated.rs
+++ src/commands/outdated.rs
@@ -1,7 +1,47 @@
-use crate::{config::Server, modrinth};
+use crate::{
+    config::{Content, Server},
+    modrinth,
+};
 use color_eyre::eyre::Result;
+use futures::stream::{self, StreamExt, TryStreamExt};
 use heck::ToTitleCase;
 use yansi::Paint;
+
+async fn run_item(server: &Server, item: &Content) -> Result<()> {
+    let latest_version = modrinth::get_latest_project_version(
+        &item.id,
+        &server.cfg.loader,
+        &server.cfg.game_version,
+    )
+    .await?;
+
+    if latest_version.id == item.version {
+        println!(
+            " {} {} {}",
+            "✔".green(),
+            item.id.blue().dim(),
+            latest_version.name
+        )
+    } else {
+        let current_version = modrinth::get_project_version(
+            &item.id,
+            &item.version,
+            &server.cfg.loader,
+            &server.cfg.game_version,
+        )
+        .await?;
+
+        println!(
+            " {} {} {} -> {}",
+            "🠙".yellow(),
+            item.id.blue().dim(),
+            current_version.name.dim(),
+            latest_version.name.green()
+        )
+    }
+
+    Ok(())
+}
 
 pub async fn run(servers: Vec<Server>, server: Option<String>) -> Result<()> {
     let servers = match server {
@@ -20,39 +60,11 @@ pub async fn run(servers: Vec<Server>, server: Option<String>) -> Result<()> {
             server.name.to_title_case().blue().bold().underline(),
         );
 
-        for item in server.cfg.content {
-            let latest_version = modrinth::get_latest_project_version(
-                &item.id,
-                &server.cfg.loader,
-                &server.cfg.game_version,
-            )
+        stream::iter(&server.cfg.content)
+            .map(|item| run_item(&server, item))
+            .buffer_unordered(8)
+            .try_collect::<Vec<_>>()
             .await?;
-
-            if latest_version.id == item.version {
-                println!(
-                    " {} {} {}",
-                    "✔".green(),
-                    item.id.blue().dim(),
-                    latest_version.name
-                )
-            } else {
-                let current_version = modrinth::get_project_version(
-                    &item.id,
-                    &item.version,
-                    &server.cfg.loader,
-                    &server.cfg.game_version,
-                )
-                .await?;
-
-                println!(
-                    " {} {} {} -> {}",
-                    "🠙".yellow(),
-                    item.id.blue().dim(),
-                    current_version.name.dim(),
-                    latest_version.name.green()
-                )
-            }
-        }
 
         if index != server_count - 1 {
             println!();
